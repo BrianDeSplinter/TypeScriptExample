13f6b50b671d47e39ff01a46d7efc66aa648a87f
BrianDeSplinter <bdesplinter34@gmail.com>
2020-07-02 12:46:46 -0700

    Added more data types

--- example.js
+++ example.js
@@ -1,1 +1,26 @@
-console.log('Hello World');
+var myString;
+// concatenations ok 'Hello' + 'world"
+// function return 'Hello'.splice(0,3)
+var myNum;
+// positive and negative numbers ok
+// decimals 5.5
+// expression 5 + 5
+var myBool;
+var myVar;
+//accepts any data type
+var strArr;
+//needs an array of strings
+var numArr;
+//needs an array numbers
+myString = 'Fireworks';
+myNum = 1776;
+myBool = true;
+myVar = "Happy 4th everyone!";
+strArr = ['Stay', 'safe'];
+numArr = [7, 4, 1776];
+console.log('String:', myString);
+console.log('Number:', myNum);
+console.log('Boolean:', myBool);
+console.log('Any:', myVar);
+console.log('String Array:', strArr);
+console.log('Number Array:', numArr);
